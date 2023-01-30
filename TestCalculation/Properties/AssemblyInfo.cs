d3fc91af67b2d07d35d66923a41c1be36b057c6f
Artem Sergeichev <a.sergeichev03@mail.ru>
2023-01-30 13:17:23 +0300

    Добавьте файлы проекта.

---
+++ TestCalculation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestCalculation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestCalculation")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8ddf40c0-1dfe-421a-adea-62e788404986")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WSUniversalLib/Calculation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WSUniversalLib
+{
+    public class Calculation
+    {
+        public int GetQuantityForProduct(int productType, int materialType, int count, float width, float length)
+        {
+            if (productType != 1 && productType != 2 && productType != 3)
+            {
+                return -1;
+            }
+
+            if (materialType != 1 && materialType != 2)
+            {
+                return -1;
+            }
+
+            double[] coefficientProductType = new double[] { 1.1, 2.5, 8.43 }; 
+            double[] percentDefectiveMaterialType = new double[] { 0.3, 0.12 }; 
+
+            double qualityRawMaterial = count * coefficientProductType[productType-1] * width * length;
+
+            double defectiveMaterial = qualityRawMaterial * percentDefectiveMaterialType[materialType-1] / 100;
+
+            int totalMaterial = Convert.ToInt32(qualityRawMaterial + defectiveMaterial);
+
+            return totalMaterial;
+        }
+    }
+}
